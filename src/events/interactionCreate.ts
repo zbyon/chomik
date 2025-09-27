4decc98c63dd11bdc9c2a3762e6389733fbf987d
Piecuuu <56731916+Piecuuu@users.noreply.github.com>
2025-09-27 12:59:19 +0200

    hopefully add database

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2025 Piecuuu
+
+ * This file is part of Chomik.
+ * Chomik is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Chomik is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Chomik. If not, see <https://www.gnu.org/licenses/>. 
+ */
+
 import { ArgsOf, Discord, On } from "discordx";
 import { Main } from "../main.js";
 import { Interaction } from "discord.js";
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+/*
+ * Copyright (C) 2025 Piecuuu
+
+ * This file is part of Chomik.
+ * Chomik is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Chomik is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Chomik. If not, see <https://www.gnu.org/licenses/>. 
+ */
+
+import "dotenv/config"
 import { Client } from "discordx"
 import { IntentsBitField } from "discord.js"
 import { dirname, importx } from "@discordx/importer";
+import { Database } from "./database.js";
 
 export class Main {
   private static _client: Client
@@ -10,6 +21,8 @@ export class Main {
   }
 
   static async start(): Promise<void> {
+    Database.connect();
+
     this._client = new Client({
       botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
       intents: [
