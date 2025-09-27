76b5b274139412a18e7514211028dde9e8e0ded3
zbyon <102828093+zbyon@users.noreply.github.com>
2025-09-27 15:13:05 +0200

    Update say.ts
    c

--- src/commands/say.ts
+++ src/commands/say.ts
@@ -6,6 +6,7 @@
  * Chomik is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with Chomik. If not, see <https://www.gnu.org/licenses/>. 
  */
+
 import { ChatInputCommandInteraction, Message } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
 
