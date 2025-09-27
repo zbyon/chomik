5853dd0df065a9490c2540de7abe49faa69f5461
Piecuuu <56731916+Piecuuu@users.noreply.github.com>
2025-09-27 15:29:52 +0200

    fix say.ts

--- src/commands/say.ts
+++ src/commands/say.ts
@@ -1,13 +1,14 @@
 /*
  * Copyright (C) 2025 zbyon
-
+ * Copyright (C) 2025 Piecuu
+ * 
  * This file is part of Chomik.
  * Chomik is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  * Chomik is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * You should have received a copy of the GNU General Public License along with Chomik. If not, see <https://www.gnu.org/licenses/>. 
  */
 
-import { ChatInputCommandInteraction, Message } from "discord.js";
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, Message } from "discord.js";
 import { Discord, Slash, SlashOption } from "discordx";
 
 @Discord()
@@ -21,7 +22,7 @@ export class SayCommand {
       name: "tekst",
       description: "Wiadomość, którą bot ma wysłać",
       required: true,
-      type: "STRING",
+      type: ApplicationCommandOptionType.String,
     })
     tekst: string,
     interaction: ChatInputCommandInteraction
