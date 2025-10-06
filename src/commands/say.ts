22e77e11964bb70e6a9fbcb3d86dc60d7eae7574
Piecuuu <56731916+Piecuuu@users.noreply.github.com>
2025-10-06 23:25:07 +0200

    say: make it technically accessible to everyone for fun

--- src/commands/say.ts
+++ src/commands/say.ts
@@ -9,10 +9,29 @@
  */
 
 import { ApplicationCommandOptionType, ChatInputCommandInteraction, BaseGuildTextChannel, Message, ReplyOptions, MessageFlags } from "discord.js";
-import { Discord, Slash, SlashOption } from "discordx";
+import { Discord, Once, Slash, SlashOption } from "discordx";
 
 @Discord()
 export class SayCommand {
+  protected static _echoUsers: string[] = ["828959544226742282", "633313654283960330", "678191118063632388"];
+  protected static _lures: string[] = [
+    "https://media.discordapp.net/attachments/1121866604431540274/1136089357598605393/attachment.gif?ex=68e503ea&is=68e3b26a&hm=6f7a4c5f19a52bad90c6ae48f1047d03033a1f4c5d2a71cb9eebadbec8dc4ad8&",
+    "https://media.discordapp.net/attachments/834183600282665041/1105496890654588940/attachment-19-1.gif?ex=68e51f35&is=68e3cdb5&hm=73d8637ef94bd56c7f25ad50dd35a3cbb45a3bda1ce2cc0d52c7cdac29a91c14&",
+    "https://cdn.discordapp.com/attachments/438496576944472075/1272677715715035157/ezgif-6-f11b581713.gif?ex=68e4e63c&is=68e394bc&hm=80f74647c114549ed40a5c36ec98bbd6f2bb63f4ef1ace97e8eb5884bb3d01ca&",
+    "https://tenor.com/view/onimai-mahiro-oyama-pout-speech-bubble-gif-27577074",
+    "https://tenor.com/view/your-package-came-in-the-mail-euphemism-homophones-pun-boykisser-gif-4464700753050259448",
+    "https://cdn.discordapp.com/attachments/849236767999787039/1302718778945306734/New_Element_14_Copy_Copy.gif?ex=68e56c68&is=68e41ae8&hm=7431a274a16e096ee5112ebff01b515da8b5a763a2ce7e4bfe2adb0d490833de&",
+    "https://media.discordapp.net/attachments/954046292584194081/1190295605893922867/RDT_20231121_1847487338397723261121471.gif?ex=68e51f2b&is=68e3cdab&hm=98eb5cbaceea966b31b1a055cff8230c4efd3011d8f5eb66c08bbb4e20373b62&",
+    "https://tenor.com/view/femboy-thighs-gif-24834955",
+    "https://tenor.com/view/boykisser-gif-17337070286879071820",
+    "https://cdn.discordapp.com/attachments/1295466439280431225/1322389172170002442/attachment.gif?ex=68e52223&is=68e3d0a3&hm=b566cac719df6e9598911651d5c4b18b276f0f58bf9598663f4cc3ed02a097ac&",
+    "https://tenor.com/view/furry-femboy-speechbubble-gif-2755355702946194600",
+    "https://tenor.com/view/femboy-opsoncelulmeu-dav6e-shanemills-katze-gif-1277417407165987518",
+  ];
+
+  private static _recentUsers: Map<string, string> = new Map<string, string>() // userid, guildid
+  private static _lastUser: { uid: string, gid: string };
+
   @Slash({
     name: "say",
     description: "Wypowiedź"
@@ -37,13 +56,28 @@ export class SayCommand {
     await interaction.deferReply({
       flags: MessageFlags.Ephemeral
     })
+    let should_pass: boolean = false;
 
-    const echo_users = ["828959544226742282", "633313654283960330"];
-    if(!echo_users.includes(interaction.member?.user.id!)) {
-      return await interaction.editReply({
-        content: "ez masz bana"
+    if((Math.floor(Math.random()*100) % 3) === 0) should_pass = true
+
+    if(SayCommand._lastUser.gid === interaction.guildId && SayCommand._lastUser.uid === interaction.user.id) should_pass = false;
+    if(SayCommand._recentUsers.get(interaction.user.id) === interaction.guildId) should_pass = false;
+
+    const forced_pass: boolean = SayCommand._echoUsers.includes(interaction.member?.user.id!);
+
+    if(!(should_pass || forced_pass)) {
+      await interaction.editReply({
+        content: SayCommand._lures[Math.floor(Math.random()*100) % SayCommand._lures.length]
       })
+      return
     }
+
+    // wasnt forced by echousers
+    if(!forced_pass) {
+      SayCommand._lastUser = { gid: interaction.guildId!, uid: interaction.user.id };
+      SayCommand._recentUsers.set(interaction.user.id, interaction.guildId!);
+    }
+
     if (interaction.isCommand()) {
       await interaction.editReply({
         content: "ok"
@@ -69,5 +103,14 @@ export class SayCommand {
       content: message,
       reply
     })
+  }
+
+  @Once({
+    event: "clientReady"
+  })
+  private clearRecordsTimeoutIgniter() {
+    setInterval(() => {
+      SayCommand._recentUsers.clear();
+    }, 3*60*1000)
   }
 }
